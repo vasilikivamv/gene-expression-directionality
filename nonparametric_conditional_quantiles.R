9874549bd6d5a90a25332e6a9d56b40782c5ba34
Vasiliki Vamvaka <47465109+vasilikivamv@users.noreply.github.com>
2021-11-11 14:37:05 +1100

    Update nonparametric_conditional_quantiles.R

--- nonparametric_conditional_quantiles.R
+++ nonparametric_conditional_quantiles.R
@@ -6,15 +6,18 @@ library(ggplot2)
 library(ggpubr)
 library(kdecopula)
 library(copula)
-library(VC2copula)
-net1<- read.delim(file="D:/DREAM5_data/Network1/input data/net1_expression_data.tsv",
+
+net1<- read.delim(file=".../.../Network1/input data/net1_expression_data.tsv",
                   header = TRUE, sep = "\t")
-gold<- read.delim(file="D:/DREAM5_data/Network1/gold standard/DREAM5_NetworkInference_GoldStandard_Network1.tsv",
+gold <- read.delim(file=".../.../Network1/gold standard/DREAM5_NetworkInference_GoldStandard_Network1.tsv",
                   header = FALSE, sep = "\t")
-used <- gold %>% subset(V3==1) %>% select(V1,V2)
-j=4004
+interactions <- gold %>% subset(V3==1) %>% select(V1,V2)
+
+# give a value at j to choose a pair of genes
 genes <- data.frame(net1 %>% select_if(used$V1[j]==colnames(net1)),
                     net1 %>% select_if(used$V2[j]==colnames(net1)))
+# inspect the data
+plot(genes)
 
 # Create the copula 
 
@@ -24,19 +27,20 @@ udat = pobs(genes)
 U <- udat[,1]        # marginal U ~ Uniform (0,1)  
 V <- udat[,2]        # marginal V ~ Uniform (0,1)
 
+# fits a nonparametric kernel density estimate for a copula
 fit <- kdecop(udat)
 summary(fit)
 
-plot(genes)
-#plot(fit)
+# plot the dependence structure
+plot(fit)
 contour(fit, col = "blue")
 
-# Step 2: Set the quantile value
+# Set the quantile values
 p = c(0.01,0.05,0.1,0.9,0.95,0.99)
 
 
 
-
+# Conditional copula V|U=u
 # C2|1 (v|u) ---> V|U=u
 u2 <- seq(0,1, length.out = length(U))
 h21 <- matrix(nrow = length(u2), ncol = length(p))
@@ -46,6 +50,7 @@ for (j in 1:length(p)) {
   }
   colnames(h21) <- c("1%","5%","10%","90%","95%","99%")
 }
+# plot
 matplot(h21, type = "l", lty = 1, lwd = 2,
         col = (cols <- seq_len(ncol(h21))), ylab = "",
         xlab = substitute(C["2|1"](v~"|"~u)~"as a function of"~
@@ -56,8 +61,8 @@ legend("bottomright", bty = "n", lwd = 2, col = cols,
 
 
 
-
-# # C1|2 (u|v) ---> U|V=v
+# Conditional copula U|V=v
+# C1|2 (u|v) ---> U|V=v
 u1 <- seq(0,1, length.out = length(U))
 # Step 3: Fitting the quantile regression model:
 h12 <- matrix(nrow = length(u1), ncol = length(p))
@@ -67,6 +72,8 @@ for (j in 1:length(p)) {
   }
   colnames(h12) <- c("1%","5%","10%","90%","95%","99%")
 }
+
+# plot
 matplot(h12, type = "l", lty = 1, lwd = 2,
         col = (cols <- seq_len(ncol(h12))), ylab = "",
         xlab = substitute(C["1|2"](u~"|"~v)~"as a function of"~
@@ -119,8 +126,6 @@ legend("bottomright", bty = "n", lwd = 2, col = cols,
        legend = as.expression(lapply(seq_along(p), function(j)
          substitute(p[1] == u1, list(u1 = p[j])))))
 
-mean(h21_inv)
-mean(h12_inv)
 
 c(mean(h21_inv[,1]),mean(h21_inv[,2]),mean(h21_inv[,3]),mean(h21_inv[,4]),mean(h21_inv[,5]),mean(h21_inv[,6]))
 
